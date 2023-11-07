d3c1b5b2836a3c95b9cd55bb761add00e383d174
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-11-07 18:58:46 +0530

    CSV File uploading Section Created in Material page

--- Material_Sheet.php
+++ Material_Sheet.php
@@ -1,3 +1,58 @@
+<?php
+function get_html($csv_file)
+{
+    $html = '<table border="1">';
+    $file = fopen($csv_file, 'r');
+    $header_arr = fgetcsv($file);
+    $html .= '<thead>';
+
+    foreach ($header_arr as $k => $v) {
+        $html .= '<th>' . $v . '</th>';
+    }
+
+    $html .= '<thead>';
+
+    $html .= '<tbody>';
+
+    while ($line = fgetcsv($file)) {
+        $html .= '<tr>';
+        foreach ($line as $k => $v) {
+            $html .= '<td>' . $v . '</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    $html .= '</tbody>';
+
+    $html .= '</thead>';
+    return $html;
+}
+
+//test
+
+
+?>
+
+
+<?php
+
+$display_table = '';
+if (isset($_POST['upload']) && $_POST['upload'] == 'upload csv') {
+
+    $upload_dir = getcwd() . DIRECTORY_SEPARATOR . '/uploads';
+    if ($_FILES['csv']['error'] == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES['csv']['tmp_name'];
+        $name = basename($_FILES['csv']['name']);
+        $csvfile = $upload_dir . '/' . $name;
+        move_uploaded_file($tmp_name, $csvfile);
+        echo 'uploaded';
+        $display_table = get_html($csvfile);
+    }
+}
+
+?>
+
+
 <!-- PAGE-HEADER -->
 <div class="page-header">
     <div>
@@ -13,45 +68,26 @@
 <!-- PAGE-HEADER END -->
 
 <div class="mb-3">
-  <label for="formFile" class="form-label">Download Material Sheet Format</label>
-  <a href="Material Sheet Format.csv" class="btn btn-primary" download>Download </a>
+    <label for="formFile" class="form-label">Download Material Sheet Format</label>
+    <a href="Material Sheet Format.csv" class="btn btn-primary" download>Download </a>
 </div><br>
 
 <div class="mb-3">
-  <label for="formFile" class="form-label">Upload Material Sheet</label>
-  <input class="form-control" type="file" id="formFile">
-  <br>
-  <button type="button" class="btn btn-primary">Upload</button>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="formFile" class="form-label">Upload Material Sheet</label>
+        <input class="form-control" type="file" id="formFile" name="csv">
+        <br>
+        <input type="submit" name="upload" value="upload csv" />
+    </form>
+
 </div>
 
 <caption>Material List</caption>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<div>
+    <?php
+    if (strlen($display_table) > 0) {
+        echo $display_table;
+    }
+    ?>
+</div>
