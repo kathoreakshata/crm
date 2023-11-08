c6819e61ae704b37428923d3c665a50f0cb49834
kathoreakshata <digital.cmq@gmail.com>
2023-11-08 16:45:43 +0530

    color logo and favicon change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Waste Mart CRM</title>
+    <title>BWM CRM</title>
 
     <!-- FAVICON -->
     <link rel="shortcut icon" type="image/x-icon" href="assets\images\favicon\favicon.ico" />
