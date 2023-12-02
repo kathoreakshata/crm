1e3d8ebb4bfca06f95aefbef36853712034fba4c
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-12-02 12:02:57 +0530

    uploads final material sheet

--- Material_Sheet.php
+++ Material_Sheet.php
@@ -1,52 +1,3 @@
-<?php
-function get_html($csv_file)
-{
-    $html = '<table border="1">';
-    $file = fopen($csv_file, 'r');
-    $header_arr = fgetcsv($file);
-    $html .= '<thead>';
-    $html .= '<tr>'; // Add a row for the header
-
-    foreach ($header_arr as $k => $v) {
-        $html .= '<th>' . $v . '</th>';
-    }
-
-    $html .= '</tr>'; // Close the header row
-    $html .= '</thead>';
-
-    $html .= '<tbody>';
-
-    while ($line = fgetcsv($file)) {
-        $html .= '<tr>';
-        foreach ($line as $k => $v) {
-            $html .= '<td>' . $v . '</td>';
-        }
-        $html .= '</tr>';
-    }
-
-    $html .= '</tbody>';
-
-    $html .= '</table>'; // Close the table
-    return $html;
-}
-
-//test
-?>
-
-<?php
-$display_table = '';
-if (isset($_POST['upload']) && $_POST['upload'] == 'upload csv') {
-    $upload_dir = getcwd() . DIRECTORY_SEPARATOR . 'uploads';
-    if ($_FILES['csv']['error'] == UPLOAD_ERR_OK) {
-        $tmp_name = $_FILES['csv']['tmp_name'];
-        $name = basename($_FILES['csv']['name']);
-        $csvfile = $upload_dir . DIRECTORY_SEPARATOR . $name;
-        move_uploaded_file($tmp_name, $csvfile);
-        $display_table = get_html($csvfile);
-    }
-}
-?>
-
 
 <!-- PAGE-HEADER -->
 <div class="page-header">
@@ -76,13 +27,13 @@ if (isset($_POST['upload']) && $_POST['upload'] == 'upload csv') {
                 <h3 class="card-title">Material Sheet Generate</h3>
             </div>
             <div class="card-body">
-                <form action="database.php" method="post" enctype="multipart/form-data"
+                <form action="index.php?Material_Sheet" method="post" enctype="multipart/form-data"
                     class="row g-3 needs-validation" novalidate>
 
                     <div class="col-md-4">
                         <div class="form-group">
                             <label class="form-label">User Name</label>
-                            <input type="text" name="Username" class="form-control" id="validationCustom01" required>
+                            <input type="text" name="username" class="form-control" id="validationCustom01" required>
                             <div class="valid-feedback">
                             User Name
                             </div>
@@ -90,7 +41,7 @@ if (isset($_POST['upload']) && $_POST['upload'] == 'upload csv') {
                     </div>
                     <div class="col-md-4">
                         <label for="validationCustom01" class="form-label">Company Name</label>
-                        <input type="text" name="Companyname" class="form-control" id="validationCustom01"  required>
+                        <input type="text" name="companyname" class="form-control" id="validationCustom01"  required>
                         <div class="valid-feedback">
                             Company Name
                         </div>
@@ -98,7 +49,7 @@ if (isset($_POST['upload']) && $_POST['upload'] == 'upload csv') {
 
                     <div class="col-4">
                         <label for="formFile" class="form-label">Upload Material Sheet</label>
-                        <input class="form-control" type="file" id="formFile" name="csv">
+                        <input class="form-control" type="file" id="formFile" name="csvfile">
                         <br>
                     </div>
                     <div class="col-12">
@@ -123,32 +74,51 @@ if (isset($_POST['upload']) && $_POST['upload'] == 'upload csv') {
 </div>
 <!-- ROW CLOSED -->
 
-<!-- Row -->
-<div class="row row-sm">
-    <div class="col-lg-12">
-        <div class="card custom-card">
-            <div class="card-header border-bottom">
-                <h3 class="card-title" b>Material Sheet</h3>
-            </div>
-            <div class="card-body">
-                <!-- <p class="text-muted">Add borders on all sides of the table and cells.</p> -->
-                <div class="table-responsive">
-                    <table class="table text-nowrap text-md-nowrap table-bordered">
-                        <thead>
-                            <tr>
-                                <th>
-                                    <?php
-                                    if (strlen($display_table) > 0) {
-                                        echo $display_table;
-                                    }
-                                    ?>
-                                </th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Row -->
+
+
+<?php
+include("connection/connectdb.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $username = $_POST["username"];
+    $companyname = $_POST["companyname"];
+
+    // Generate a unique identifier based on username and timestamp
+    $uniqueId = $companyname . "_" . time();
+
+    // Process CSV file
+    $csvFileName = $uniqueId . ".csv";
+    $csvTempName = $_FILES["csvfile"]["tmp_name"];
+
+    // Check if a file was uploaded
+    if (is_uploaded_file($csvTempName)) {
+        // Read CSV file content
+        $csvContent = file_get_contents($csvTempName);
+
+        // Append data to CSV file (you may want to customize the file path)
+        $filePath = "uploads/" . $csvFileName;
+        file_put_contents($filePath, $csvContent, FILE_APPEND);
+
+        // Store other form data in the CSV file
+        $dataToStore = "ID,Username,Company\n"; // Header for CSV file
+        $dataToStore .= "$uniqueId,$username,$companyname\n"; // Data to be stored
+        file_put_contents($filePath, $dataToStore, FILE_APPEND);
+
+        // If you are using a database, you can insert data here
+        // For example:
+        $sql = "INSERT INTO mastersheet (id, username, company, csv_file) VALUES ('$uniqueId', '$username', '$companyname', '$csvFileName')";
+        $con->query($sql);
+
+        echo "<h4 style='color:#047cbc;'>Material Sheet submitted successfully!</h4>";
+    } else {
+        echo "<h4 style='color:#047cbc;'>Error Material Sheet submitted.</h4>";
+    }
+} else {
+    echo "<h4 style='color:#047cbc;'>Invalid request.</h4>";
+}
+
+// Close the database connection if opened
+$con->close();
+?>
+
